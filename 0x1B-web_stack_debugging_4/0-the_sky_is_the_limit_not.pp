58d6bfdbe69e37cc2425f8f185d5c06a3dfd5061
Joe-Chege <njorogejosephchege@gmail.com>
2023-11-17 11:20:36 +0300

    arrow should be on the right operand's line on line 7 (check: arrow_on_right_operand_line)

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# Increases the amount of traffic an Nginx server can handle.
-
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Exec['nginx-restart'], # Trigger the Nginx restart after this command
+}
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+  command     => '/etc/init.d/nginx restart', # Use full path to the init script
+  path        => '/sbin:/bin:/usr/sbin:/usr/bin',
+  refreshonly => true # Ensure this only runs when notified by 'fix--for-nginx'
 }
